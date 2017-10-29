3ba7ce44d45ea52bdf6fd58e3a20f97ce3f92156
Jasuutu <cloudstrife.schultz@gmail.com>
2017-10-29 17:21:34 -0400

    Adding VeggiePizza class.

---
+++ PatternExamples/FactoryExampleTests/VeggiePizzaTests.cs
@@ -0,0 +1,44 @@
+﻿using FactoryExample;
+using Xunit;
+
+namespace FactoryExampleTests
+{
+    public class VeggiePizzaTests
+    {
+        private const string VeggiePizzaAddCheeseString = "Adding cheese to the veggie pizza.";
+        private const string VeggiePizzaAddSauceString = "Adding the sauce to the veggie pizza.";
+        private const string VeggiePizzaAddToppingsString = "Adding veggies to the pizza.";
+        private const string VeggiePizzaBakePizzaString = "Baking the veggie pizza.";
+
+        private readonly PizzaBase vegiePizza;
+
+        public VeggiePizzaTests()
+        {
+            vegiePizza = new VeggiePizza();
+        }
+
+        [Fact]
+        public void AddCheeseTest()
+        {
+            Assert.True(Utility.AreEqual(vegiePizza.AddCheese(), VeggiePizzaAddCheeseString));
+        }
+
+        [Fact]
+        public void AddSauceTest()
+        {
+            Assert.True(Utility.AreEqual(vegiePizza.AddSauce(), VeggiePizzaAddSauceString));
+        }
+
+        [Fact]
+        public void AddToppingsTest()
+        {
+            Assert.True(Utility.AreEqual(vegiePizza.AddToppings(), VeggiePizzaAddToppingsString));
+        }
+
+        [Fact]
+        public void BakePizzaTest()
+        {
+            Assert.True(Utility.AreEqual(vegiePizza.BakePizza(), VeggiePizzaBakePizzaString));
+        }
+    }
+}
---
+++ PatternExamples/PatternExamples/VeggiePizza.cs
@@ -0,0 +1,25 @@
+﻿namespace FactoryExample
+{
+    public class VeggiePizza : PizzaBase
+    {
+        public override string AddCheese()
+        {
+            return "Adding cheese to the veggie pizza.";
+        }
+
+        public override string AddSauce()
+        {
+            return "Adding the sauce to the veggie pizza.";
+        }
+
+        public override string AddToppings()
+        {
+            return "Adding veggies to the pizza.";
+        }
+
+        public override string BakePizza()
+        {
+            return "Baking the veggie pizza.";
+        }
+    }
+}
