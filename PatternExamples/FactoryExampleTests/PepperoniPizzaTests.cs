d9cf8fdd79c81a94ee85fcb27ee03a324121bde4
Jasuutu <cloudstrife.schultz@gmail.com>
2017-10-29 16:50:15 -0400

    Adding Pepperoni Pizza class with tests.

---
+++ PatternExamples/FactoryExampleTests/PepperoniPizzaTests.cs
@@ -0,0 +1,42 @@
+﻿using FactoryExample;
+using Xunit;
+
+namespace FactoryExampleTests
+{
+    public class PepperoniPizzaTests
+    {
+        private const string PepperoniPizzaAddCheeseString = "Adding cheese to the pepperoni pizza.";
+        private const string PepperoniPizzaAddSauceString = "Adding sauce to the pepperoni pizza.";
+        private const string PepperoniPizzaBakePizzaString = "Baking pizza.";
+
+        private PizzaBase pepperoniPizza;
+
+        public PepperoniPizzaTests()
+        {
+            pepperoniPizza = new PepperoniPizza();
+        }
+
+        [Fact]
+        public void AddCheeseTest()
+        {
+            Assert.True(AreEqual(pepperoniPizza.AddCheese(), PepperoniPizzaAddCheeseString));
+        }
+
+        [Fact]
+        public void AddSauceTest()
+        {
+            Assert.True(AreEqual(pepperoniPizza.AddSauce(), PepperoniPizzaAddSauceString));
+        }
+
+        [Fact]
+        public void BakePizzaTest()
+        {
+            Assert.True(AreEqual(pepperoniPizza.BakePizza(), PepperoniPizzaBakePizzaString));
+        }
+
+        private bool AreEqual(string inputString, string constantString)
+        {
+            return inputString == constantString;
+        }
+    }
+}
---
+++ PatternExamples/PatternExamples/PepperoniPizza.cs
@@ -0,0 +1,20 @@
+﻿namespace FactoryExample
+{
+    public class PepperoniPizza : PizzaBase
+    {
+        public override string AddCheese()
+        {
+            return "Adding cheese to the pepperoni pizza.";
+        }
+
+        public override string AddSauce()
+        {
+            return "Adding sauce to the pepperoni pizza.";
+        }
+
+        public override string BakePizza()
+        {
+            return "Baking pizza.";
+        }
+    }
+}
